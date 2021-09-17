2a223ca3b7aeb5890b180c1ec1c2202d2b583029
Orion412 <gart.alt1@gmail.com>
2021-09-17 18:18:52 -0500

    portfolio overhaul

---
+++ script.js
@@ -0,0 +1,68 @@
+class Scrambler {
+  constructor(qs) {
+    this.qs = qs;
+    this.chars = "!<>-_/[]{}-=+*^?#@&$%________";
+    this.update = this.update.bind(this);
+  }
+  setText(newText) {
+    const oldText = this.qs.innerText;
+    const length = Math.max(oldText.length, newText.length);
+    const promise = new Promise((resolve) => (this.resolve = resolve));
+    this.queue = [];
+    for (let i = 0; i < length; i++) {
+      const from = oldText[i] || "";
+      const to = newText[i] || "";
+      const start = Math.floor(Math.random() * 40);
+      const end = start + Math.floor(Math.random() * 40);
+      this.queue.push({ from, to, start, end });
+    }
+    cancelAnimationFrame(this.frameRequest);
+    this.frame = 0;
+    this.update();
+    return promise;
+  }
+  update() {
+    let output = "";
+    let complete = 0;
+    for (let i = 0, n = this.queue.length; i < n; i++) {
+      let { from, to, start, end, char } = this.queue[i];
+      if (this.frame >= end) {
+        complete++;
+        output += to;
+      } else if (this.frame >= start) {
+        if (!char || Math.random() < 0.28) {
+          char = this.randomChar();
+          this.queue[i].char = char;
+        }
+        output += `<span class="trans">${char}</span>`;
+      } else {
+        output += from;
+      }
+    }
+    this.qs.innerHTML = output;
+    if (complete === this.queue.length) {
+      this.resolve();
+    } else {
+      this.frameRequest = requestAnimationFrame(this.update);
+      this.frame++;
+    }
+  }
+  randomChar() {
+    return this.chars[Math.floor(Math.random() * this.chars.length)];
+  }
+}
+
+const greetings = ["Hello World!", "Welcome!","Hire Me!"];
+
+const qs = document.querySelector(".cycleText");
+const scrmbl = new Scrambler(qs);
+
+let counter = 0;
+const next = () => {
+  scrmbl.setText(greetings[counter]).then(() => {
+    setTimeout(next, 800);
+  });
+  counter = (counter + 1) % greetings.length;
+};
+
+next();
